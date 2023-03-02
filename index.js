d4ecef85365fded8fd1d87199ecb4f16127ebf82
pikkari <joonas.jarvinen@opiskelija.lhkk.fi>
2023-03-02 12:29:52 +0200

    Test

---
+++ index.js
@@ -0,0 +1,75 @@
+require('dotenv').config()
+
+console.log(process.env)
+
+const Discord = require('discord.js');
+
+const { Client, GatewayIntentBits, Events } = require('discord.js');
+const client = new Client({
+    intents: [
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildVoiceStates,
+    ],
+});
+
+client.on("ready", function () {
+    console.log("Ready to begin! Serving in " + client.channels.length + " channels");
+});
+
+client.on('messageCreate', msg => {
+
+    if (msg.author.id !== client.user.id) {
+        if(msg.content.indexOf("Smash") === 0)
+        {
+        console.log(msg.author.id);
+        //msg.channel.send(msg.content.split('').reverse().join(''));
+        msg.channel.send("bing chilling!");
+    }
+    }
+});
+
+const { DisTube } = require("distube");
+
+client.DisTube = new DisTube(client, {
+    leaveOnStop: false,
+    emitNewSongOnly: true,
+    emitAddSongWhenCreatingQueue: false,
+    emitAddListWhenCreatingQueue: false,
+})
+
+client.on('messageCreate', message => {
+    if (message.author.bot || !message.guild) return;
+    const prefix = "/"
+    const args = message.content.slice(prefix.length).trim().split(/ +/g)
+
+    if (!message.content.startsWith(prefix)) return;
+
+    if (args.shift().toLowerCase() === "play") {
+        client.DisTube.play(message.member.voice.channel, args.join(""), {
+            member: message.member,
+            textChannel: message.channel,
+            message
+        })
+    }
+})
+
+client.DisTube.on("playSong", (queue, song) => {
+    queue.textChannel.send("Nyt Soi: " + song.name)
+})
+
+client.on("disconnected", function () {
+    // alert the console
+    console.log("Disconnected!");
+
+    // exit node.js with an error
+    process.exit(1);
+});
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.login(process.env.TOKEN);
---
+++ speudo.md
@@ -0,0 +1,39 @@
+Liittyy kanavalle:
+
+join channel {...}
+
+Arpoo ajan:
+
+Random timer (30m-90m) {
+...
+}
+
+Aika on loppunut (arpoo toisen ajan):
+
+When time is up {
+Random timer2 (15s-30s)
+}
+
+arpo biisin:
+
+
+
+
+
+
+
+
+
+Soittaa musiikkia
+
+
+
+play song (...) {
+    get timer
+}
+
+timer {
+    get time (12-30sec)
+}
+
+
